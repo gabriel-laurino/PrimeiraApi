0ab83431522233dbd96824276d026b0ad45422d0
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-05-10 09:05:06 -0300

    ...

--- Requisições.py
+++ Requisições.py
@@ -5,7 +5,7 @@ import base64
 chave_path = "chave.pem"
 
 with open(chave_path, "rb") as cert_file:
-    chave_base64 = base64.b64encode(cert_file.read()).decode('utf-8')
+    chave_base64 = base64.b64encode(cert_file.read()).decode("utf-8")
 
 url = "http://localhost:5184/login"
 
@@ -13,7 +13,7 @@ payload = {
     "username": "nomeusuario",
     "email": "email@dominio.com",
     "senha": "senha123",
-    "chave": chave_base64 
+    "chave": chave_base64,
 }
 
 data = json.dumps(payload)
@@ -24,12 +24,11 @@ response = requests.post(url, data=data, headers=headers)
 
 if response.status_code == 200:
     json_response = response.json()
-    token = json_response.get('token')
+    token = json_response.get("token")
     print(f"Login bem sucedido! \n\nToken: {token}")
 else:
-    error_message = response.content.decode('utf-8')
+    error_message = response.content.decode("utf-8")
     print("Resposta completa:\n", error_message)
 
-    first_line = error_message.split('\n')[0]
+    first_line = error_message.split("\n")[0]
     print("Erro simplificado:\n", first_line)
-
