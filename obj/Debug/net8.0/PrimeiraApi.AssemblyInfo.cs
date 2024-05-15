6b9e147b11a0be4e4124221a59ece1ae945c30f2
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-05-15 16:36:53 -0300

    Correção NuGet

--- obj/Debug/net8.0/PrimeiraApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeiraApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiraApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab83431522233dbd96824276d026b0ad45422d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiraApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiraApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
